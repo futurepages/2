ec22097bf2dd6e767db8af58f8fe2247df7301a9
danilo <danilo@39b1b8b8-f3f2-11de-9538-e304f5d36b94>
2010-01-07 23:40:08 +0000

    extracao do projeto FutureTest

--- trunk/src/org/futurepages/test/page/html/exception/NoUniqueUIElementFoundOnPage.java
+++
@@ -1,9 +0,0 @@
-package org.futurepages.test.page.html.exception;
-
-public class NoUniqueUIElementFoundOnPage extends Exception {
-
-	public NoUniqueUIElementFoundOnPage(String elementName) {
-		super("Mais de um elemento com name='"+elementName+"' encontrados.");
-	}
-	
-}
