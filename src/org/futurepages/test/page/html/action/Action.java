9ff0d06de2b47e7191bb0d50b23b39d9c263671e
danilo <danilo@39b1b8b8-f3f2-11de-9538-e304f5d36b94>
2010-01-07 23:40:08 +0000

    extracao do projeto FutureTest

--- src/org/futurepages/test/page/html/action/Action.java
+++
@@ -1,6 +0,0 @@
-package org.futurepages.test.page.html.action;
-
-public interface Action {
-
-	public void act(String name, Object... args);
-}
--- src/org/futurepages/test/page/html/exception/UIElementEnumUndentifiedException.java
+++
@@ -1,8 +0,0 @@
-package org.futurepages.test.page.html.exception;
-
-public class UIElementEnumUndentifiedException extends RuntimeException{
-
-	public UIElementEnumUndentifiedException(String name) {
-		super("Nenhum UIElement identificado com name: '"+name+"'.");
-	}
-}
