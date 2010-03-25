1ad447bdeff1059dab52b4f22cda8a03eb6fbc82
danilo <danilo@39b1b8b8-f3f2-11de-9538-e304f5d36b94>
2010-03-25 21:53:24 +0000

    PersistenceInjectionFilter

---
+++ test/modules/escola/beans/Turma.java
@@ -0,0 +1,37 @@
+package modules.escola.beans;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Turma implements Serializable{
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private long id;
+
+	private String turma;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getTurma() {
+		return turma;
+	}
+
+	public void setTurma(String turma) {
+		this.turma = turma;
+	}
+	
+
+	
+}
--- test/org/futurepages/FuturePagesTestSuite.java
+++ test/org/futurepages/FuturePagesTestSuite.java
@@ -4,6 +4,7 @@ import org.futurepages.core.persistence.HQLFieldTest;
 import org.futurepages.core.persistence.HQLProviderTest;
 import org.futurepages.core.persistence.HibernateFilterTest_isTransactional;
 import org.futurepages.enums.EnumTestSuite;
+import org.futurepages.filters.FilterTestSuite;
 import org.futurepages.tags.TagsTestSuite;
 import org.futurepages.util.UtilTestSuite;
 import org.junit.runner.RunWith;
@@ -13,6 +14,7 @@ import org.junit.runners.Suite.SuiteClasses;
 @RunWith(Suite.class)
 @SuiteClasses({
 	EnumTestSuite.class,
+	FilterTestSuite.class,
 	UtilTestSuite.class,
 	TagsTestSuite.class,
 	HQLProviderTest.class,
