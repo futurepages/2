92442d4db3e18114611bc59d70edf4e3dce3b959
danilo <danilo@39b1b8b8-f3f2-11de-9538-e304f5d36b94>
2010-06-01 23:20:49 +0000

    refatoracao de tags else, then

--- src/org/futurepages/tags/core/conditional/Else.java
+++ src/org/futurepages/tags/core/conditional/Else.java
@@ -1,43 +1,13 @@
 package org.futurepages.tags.core.conditional;
 
-import javax.servlet.jsp.JspException;
-import javax.servlet.jsp.tagext.Tag;
-
 import org.futurepages.core.tags.ConditionalTag;
 import org.futurepages.core.tags.build.ContentTypeEnum;
 
 @org.futurepages.annotations.Tag(bodyContent = ContentTypeEnum.JSP)
-
-public class Else extends IfTag {
-	
-	public int doStartTag() throws JspException {
-		init();
-		eval();
-		if(this.testCondition() && (this.context || !print)){
-			return EVAL_BODY_BUFFERED;
-		}
-		return SKIP_BODY;
-	}
+public class Else extends ConditionalContextTag {
 	
 	@Override
-	public boolean testCondition() throws JspException {
-		Tag parent = findAncestorWithClass(this, ConditionalTag.class);
-		if (parent != null) {
-			ConditionalTag conditional = (ConditionalTag) parent;
-			boolean result = checkParent(conditional);
-			return result;
-		} else {
-			throw new JspException("Then not enclosed by a Conditinal tag!");
-		}
-	}
-
-	private boolean checkParent(ConditionalTag conditional) {
-		System.out.println("Else.checkParent()  "+this.getId());
-		System.out.println("pai (	"+conditional.getClass().getSimpleName() + " id: "+conditional.getId() +"): "+conditional.isCondition());
-		boolean result = !conditional.isCondition();
-		System.out.println("retorno:  !pai: "+result);
-		System.out.println("---------------");
-		
-		return result;
+	protected boolean contextualCondition(ConditionalTag conditional) {
+		return  !conditional.isCondition();
 	}
 }
--- src/org/futurepages/tags/core/conditional/Then.java
+++ src/org/futurepages/tags/core/conditional/Then.java
@@ -1,33 +1,14 @@
 package org.futurepages.tags.core.conditional;
 
-import javax.servlet.jsp.JspException;
-import javax.servlet.jsp.tagext.Tag;
-
 import org.futurepages.core.tags.ConditionalTag;
 import org.futurepages.core.tags.build.ContentTypeEnum;
 
 @org.futurepages.annotations.Tag(bodyContent = ContentTypeEnum.JSP)
-public class Then extends IfTag{
-	
-	public int doStartTag() throws JspException {
-		init();
-		eval();
-		if(this.testCondition() && (this.context || !print)){
-			return EVAL_BODY_BUFFERED;
-		}
-		return SKIP_BODY;
-	}
-	
-	
+public class Then extends ConditionalContextTag{
+
 	@Override
-	public boolean testCondition() throws JspException {
-		Tag parent = findAncestorWithClass(this, ConditionalTag.class);
-		if (parent != null) {
-			ConditionalTag conditional = (ConditionalTag) parent;
-			return conditional.isCondition();
-		} else {
-			throw new JspException("Then not enclosed by a Conditinal tag! ");
-		}
+	protected boolean contextualCondition(ConditionalTag conditional) {
+		return conditional.isCondition();
 	}
 
 }
