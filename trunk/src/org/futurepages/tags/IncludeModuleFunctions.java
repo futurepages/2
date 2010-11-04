1d95c1883dda1d790a14f2969cf4f5c394388436
leandro <leandro@39b1b8b8-f3f2-11de-9538-e304f5d36b94>
2010-11-04 23:32:30 +0000

    Agora o módulo portal tem seu javascript e css automaticamente importados.

--- trunk/src/org/futurepages/tags/IncludeModuleFunctions.java
+++ trunk/src/org/futurepages/tags/IncludeModuleFunctions.java
@@ -9,7 +9,6 @@ import org.futurepages.core.tags.PrintTag;
 import org.futurepages.core.tags.build.ContentTypeEnum;
 import org.futurepages.filters.ModuleIdFilter;
 
-
 /**
  * @author Leandro
  */
@@ -19,14 +18,16 @@ public class IncludeModuleFunctions extends PrintTag{
 	@Override
 	public String getStringToPrint() throws JspException {
 		try{
+			StringBuffer sb = new StringBuffer();
+			sb.append("<script type=\"text/javascript\" src=\""+Paths.module(req,"portal")+"/template/functions.js\"></script>");
 			String moduleId =ModuleIdFilter.getModuleId(action);
-			if(moduleId!=null){
-				return "<script type=\"text/javascript\" src=\""+Paths.module(req, moduleId)+"/template/functions.js\"></script>";
+			if(moduleId!=null && !moduleId.equals("portal")){
+				sb.append("<script type=\"text/javascript\" src=\""+Paths.module(req, moduleId)+"/template/functions.js\"></script>");
 			}
+			return sb.toString();
 		}
 		catch(Exception ex){
 			return "";
 		}
-		return "";
     }
 }
