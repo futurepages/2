aeaa2212419abd11a932e9bac384751c716b794d
leandro <leandro@39b1b8b8-f3f2-11de-9538-e304f5d36b94>
2011-09-17 21:35:17 +0000

    ConsequenceCallback desenvolvido

--- trunk/src/org/futurepages/core/context/MapContext.java
+++ trunk/src/org/futurepages/core/context/MapContext.java
@@ -5,7 +5,8 @@ import java.util.Iterator;
 import java.util.Map;
 
 /**
- * A simple Context that can be used for testing.
+ * A simple Context that can be used for a lot of things.
+ * 
  * A SessionContext and an ApplicationContext can be mocked with this class.
  *
  * @author Sergio Oliveira
@@ -22,27 +23,33 @@ public class MapContext implements Context {
         this.values = values;
     }
     
+	@Override
 	public Object getAttribute(String name) {
         return values.get(name);
     }
 	
+	@Override
 	public void setAttribute(String name, Object value) {
         values.put(name, value);
     }
 	
+	@Override
 	public void removeAttribute(String name) {
         values.remove(name);
     }
 	
+	@Override
 	public Iterator<String> keys() {
 		
 		return values.keySet().iterator();
 	}
 	
+	@Override
 	public void reset() {
         values.clear();
     }
 	
+	@Override
 	public boolean hasAttribute(String name) {
         return values.containsKey(name);
     }
