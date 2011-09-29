33e8c42bd0dc5de2306ae4afead90270c85d8723
angelo <angelo@39b1b8b8-f3f2-11de-9538-e304f5d36b94>
2011-09-29 17:46:26 +0000

    Inclusão de método notIn, notExists e constante NOT_EXISTS

--- src/org/futurepages/core/persistence/HQLable.java
+++ src/org/futurepages/core/persistence/HQLable.java
@@ -14,6 +14,7 @@ public interface HQLable {
     public static final String ASC = " ASC";
     public static final String DESC = " DESC";
     public static final String IN = " IN ";
+	public static final String NOT_EXISTS = " NOT EXISTS ";
     public static final String NOT_IN = " NOT IN ";
     public static final String NOT_LIKE  = " NOT LIKE ";
     public static final String AS  = " AS ";
