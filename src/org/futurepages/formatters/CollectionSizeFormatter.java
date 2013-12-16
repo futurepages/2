cdf4669a8ec96918cd0daebc14be84d40714ff1b
leandro <leandro@39b1b8b8-f3f2-11de-9538-e304f5d36b94>
2013-12-16 03:05:48 +0000

    contemplando Map no formatador de CollectionSize

--- src/org/futurepages/formatters/CollectionSizeFormatter.java
+++ src/org/futurepages/formatters/CollectionSizeFormatter.java
@@ -2,6 +2,8 @@ package org.futurepages.formatters;
 
 import java.util.Collection;
 import java.util.Locale;
+import java.util.Map;
+
 import org.futurepages.core.formatter.Formatter;
 
 /**
@@ -13,7 +15,9 @@ public class CollectionSizeFormatter implements Formatter {
     public String format(Object value, Locale loc) {
 		if(value instanceof Object[]){
 			return String.valueOf(((Object[])value).length);
-		}
+		}else if(value instanceof Map){
+			return String.valueOf(((Map)value).size());
+	    }
         return String.valueOf(((Collection) value).size());
     }
 }
