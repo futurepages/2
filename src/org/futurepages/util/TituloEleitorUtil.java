03141f566fd7bdbe468ca79dcaf7c1303aa98105
Leandro Santana <iileandro@gmail.com>
2020-02-03 16:16:24 -0300

    get Dígito do título de eleitor

--- src/org/futurepages/util/TituloEleitorUtil.java
+++ src/org/futurepages/util/TituloEleitorUtil.java
@@ -61,4 +61,54 @@ public class TituloEleitorUtil {
 		}
 		return ((strDV1 + strDV2).equals(strDVc));
 	}
+
+	//example: 0295251315-62
+	@SuppressWarnings("Duplicates")
+	public static String getDigito(String strSequencial) {
+		int ind1, ind2, limite, soma, digito;
+		String strDVc = "";
+		String strUF = strSequencial.substring(9, 11);
+
+		// Verifca se a UF estiver entre os código possíveis, de 1(SP) até 28(ZZ-Exterior)
+		if (Integer.parseInt(strUF) > 0 && Integer.parseInt(strUF) < 29) {
+			limite = 9;
+			// Loop para calcular os 2 dígitos verificadores
+			for (ind1 = 1; ind1 <= 2; ind1++) {
+				soma = 0;
+				// Calcula a soma para submeter ao módulo 11
+				for (ind2 = 1; ind2 <= limite; ind2++) {
+					soma = soma + Integer.parseInt(strSequencial.substring((ind2 - 1), (ind2 - 1) + 1)) * (limite + 2 - (ind2));
+				}
+				// Pega o resto da dívisão, o módulo, por 11
+				digito = soma % 11;
+				// Se a UF for SP ou MG
+				if (Integer.parseInt(strUF) == 1 || Integer.parseInt(strUF) == 2) {
+					if (digito == 1) {
+						digito = 0;
+					} else if (digito == 0) {
+						digito = 1;
+					} else {
+						digito = 11 - digito;
+					}
+				} else {
+					// Outros UF e Exterior
+					if ((digito == 1) || (digito == 0)) {
+						digito = 0;
+					} else {
+						digito = 11 - digito;
+					}
+				}
+				// Atribui à variável strDVc o dígito calculado
+				strDVc = strDVc + String.valueOf(digito);
+				// Muda o valor de intLimite para o cáculo do segundo dígito
+				limite = 3;
+				// O cálculo do segundo dígito será sobre o código da UF + primeiro dígito verificador
+			}
+		}
+		return strDVc;
+	}
+
+	public static void main(String[] args) {
+		System.out.println(getDigito("29525131562"));
+	}
 }
